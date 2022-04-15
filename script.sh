04a13ed2932fd28b760f5f69086802ecc8aeaa0b
monty233038 <52647221+monty233038@users.noreply.github.com>
2022-04-15 10:38:20 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -19,3 +19,37 @@ mod hfs
 mod hfsplus
 mod udf
 mod freevxfs
+
+pvcreate /var/xvdf
+vgcreate vol /var/xvdf
+
+mount | grep /tmp
+if [ $? -eq 0 ]
+then
+	umount /tmp
+	rm -rf /tmp
+	mkdir /tmp
+	chmod 777 /tmp
+	lvcreate -L 500 -n tmp vol
+	mkfs.ext4 /dev/vol/tmp
+	echo "/dev/vol/tmp	/tmp	ext4	rw,nosuid,nodev,noexec,relatime		1	2" >> /etc/fstab
+	mount -a
+else
+
+	rm -rf /tmp
+	mkdir /tmp
+	chmod 777 /tmp
+	lvcreate -L 500 -n tmp vol
+	mkfs.ext4 /dev/vol/tmp
+	echo "/dev/vol/tmp	/tmp	ext4	rw,nosuid,nodev,noexec,relatime		1	2" >> /etc/fstab
+	mount -a
+fi
+
+mount | grep /tmp | grep nodev
+if [ $? -ne 0 ]
+then
+	awk'$1~"/dev/vol/tmp"{$4="nodev,"$4}'1 OFS="\t" /etc/fstab
+	mount -o remount,nodev /tmp
+fi
+
+
