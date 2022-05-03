08bff576c02a2118e1916315d5de3d188e7e7f60
monty233038 <52647221+monty233038@users.noreply.github.com>
2022-05-03 15:52:14 +0530

    Update cis.sh

--- cis.sh
+++ cis.sh
@@ -295,11 +295,11 @@ fi
 sleep 5
 echo -e "\e[1;31m Ensure AIDE is installed \e[0m"
 dpkg -s aide
-if [ $? -ne 0 ]
-then
+#if [ $? -ne 0 ]
+#then
 #	echo -e "y" | apt-get install aide aide-common
 #	aideinit
-fi
+#fi
 sleep 5
 
 echo -e "\e[1;31m Ensure filesystem integrity is regularly checked \e[0m"            
