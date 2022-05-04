2c63c52548e3288db1ac5e91345f0816b943b086
monty233038 <52647221+monty233038@users.noreply.github.com>
2022-05-04 18:30:51 +0530

    Update cis.sh

--- cis.sh
+++ cis.sh
@@ -293,14 +293,14 @@ then
 	fi
 fi	
 sleep 5
-echo -e "\e[1;31m Ensure AIDE is installed \e[0m"
-dpkg -s aide
-if [ $? -ne 0 ]
-then
-	export DEBIAN_FRONTEND=noninteractive
-	DEBIAN_FRONTEND=noninteractive apt-get -q -y install aide aide-common
-	aideinit
-fi
+#echo -e "\e[1;31m Ensure AIDE is installed \e[0m"
+#dpkg -s aide
+#if [ $? -ne 0 ]
+#then
+#	export DEBIAN_FRONTEND=noninteractive
+#	DEBIAN_FRONTEND=noninteractive apt-get -q -y install aide aide-common
+#	aideinit
+#fi
 sleep 5
 
 echo -e "\e[1;31m Ensure filesystem integrity is regularly checked \e[0m"            
